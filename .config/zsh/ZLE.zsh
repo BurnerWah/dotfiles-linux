b5b36bf8d8d89a26e2e3d0e79ba4fd9db22b0867
Jaden Pleasants <jadenpleasants@fastmail.com>
2019-11-21 13:03:52 -0600

    Improvements to zsh keybindings

--- .config/zsh/ZLE.zsh
+++ .config/zsh/ZLE.zsh
@@ -9,13 +9,12 @@ key[Alt]='^['
 # Function loading {{{1
 autoload -Uz lib/redraw-prompt && zle -N zle::Prompt.Redraw lib/redraw-prompt
 
-autoload -Uz widget/cd         && zle -N widget/cd
+autoload -Uz widget/cd         && zle -N widget::cd widget/cd
 autoload -Uz edit-command-line && zle -N edit-command-line
-autoload -Uz widget/ctrl-z     && zle -N widget/ctrl-z
+autoload -Uz widget/ctrl-z     && zle -N widget::CTRL-Z widget/ctrl-z
 autoload -Uz grep2awk          && zle -N grep2awk
-autoload -Uz widget/history    && zle -N widget/history
-autoload -Uz widget/sudo       && zle -N widget/sudo
-autoload -Uz widget/z          && zle -N widget/z
+autoload -Uz widget/history    && zle -N widget::hist widget/history
+autoload -Uz widget/z          && zle -N widget::z widget/z
 autoload -Uz sticky-note       && zle -N sticky-note
 
 autoload -Uz surround && {
@@ -26,7 +25,7 @@ autoload -Uz surround && {
 
 autoload -Uz widget/vim-quit && { zle -N quit widget/vim-quit && zle -A quit q } # quit like vim
 
-autoload -Uz widget/fuck &&   zle -N fuck widget/fuck
+autoload -Uz widget/fuck && zle -N fuck widget/fuck
 
 # Keybindings: {{{1
 # INSERT {{{2
@@ -36,14 +35,17 @@ bindkey -M viins "${key[Tab]}"    complete-word  # normal tab completion
 bindkey -M viins "${key[Delete]}" vi-delete-char
 bindkey -M viins "${key[Home]}"   vi-beginning-of-line
 bindkey -M viins "${key[End]}"    vi-end-of-line
-bindkey -M viins '^[c'  widget/cd # cd to a subdirectory
-bindkey -M viins '^[z'  widget/z  # jump to a folder using z.lua
-bindkey -M viins '^R'   widget/history  # input a command from shell's history
-bindkey -M viins '^Z'   widget/ctrl-z
+bindkey -M viins '^[c'  widget::cd # cd to a subdirectory
+bindkey -M viins '^[z'  widget::z  # jump to a folder using z.lua
+bindkey -M viins '^R'   widget::hist  # input a command from shell's history
+bindkey -M viins '^Z'   widget::CTRL-Z
 bindkey -M viins '\E\E' fuck  # fix last command
 # NORMAL {{{2
 bindkey -M vicmd su widget/sudo # rerun last command as root
 bindkey -M vicmd \! edit-command-line # modify the command being input in $EDITOR
 bindkey -M vicmd ZZ quit   # That's a thing you can do in vim
+bindkey -M vicmd cs change-surround
+bindkey -M vicmd ds delete-surround
+bindkey -M vicmd ys add-surround
 
 # vim:fenc=utf-8 ft=zsh fdm=marker
