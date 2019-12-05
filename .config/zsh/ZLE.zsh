79a4f42105bbb36190835a248d6bb5ee57965fe8
Jaden Pleasants <jadenpleasants@fastmail.com>
2019-12-04 23:37:09 -0600

    Reworked ZLE widgets a bit to make them release-ready

--- .config/zsh/ZLE.zsh
+++ .config/zsh/ZLE.zsh
@@ -7,14 +7,7 @@ key[Esc]='\e'
 key[Alt]='^['
 
 # Function loading {{{1
-autoload -Uz lib/redraw-prompt && zle -N zle::Prompt.Redraw lib/redraw-prompt
-
-autoload -Uz widget/cd         && zle -N widget::cd widget/cd
 autoload -Uz edit-command-line && zle -N edit-command-line
-autoload -Uz widget/ctrl-z     && zle -N widget::CTRL-Z widget/ctrl-z
-autoload -Uz grep2awk          && zle -N grep2awk
-autoload -Uz widget/history    && zle -N widget::hist widget/history
-autoload -Uz widget/z          && zle -N widget::z widget/z
 autoload -Uz sticky-note       && zle -N sticky-note
 
 autoload -Uz surround && {
@@ -23,9 +16,17 @@ autoload -Uz surround && {
   zle -N change-surround surround
 }
 
-autoload -Uz widget/vim-quit && { zle -N quit widget/vim-quit && zle -A quit q } # quit like vim
+zle -N redraw-prompt __ZLE_redraw_prompt__
 
-autoload -Uz widget/fuck && zle -N fuck widget/fuck
+zle -N rationalize-dot __ZLE_rationalize_dot__
+zle -N fzf-zlua __ZLE_fzf_zlua__
+zle -N fzf-cd __ZLE_fzf_cd__
+zle -N fzf-hist __ZLE_fzf_history__
+
+zle -N quit __ZLE_CMD_quit__ && zle -A quit q
+zle -N fuck __ZLE_CMD_fuck__
+
+zle -N _Ctrl+z __ZLE_map_ctrl+z__
 
 # Keybindings: {{{1
 # INSERT {{{2
@@ -35,13 +36,14 @@ bindkey -M viins "${key[Tab]}"    complete-word  # normal tab completion
 bindkey -M viins "${key[Delete]}" vi-delete-char
 bindkey -M viins "${key[Home]}"   vi-beginning-of-line
 bindkey -M viins "${key[End]}"    vi-end-of-line
-bindkey -M viins '^[c'  widget::cd # cd to a subdirectory
-bindkey -M viins '^[z'  widget::z  # jump to a folder using z.lua
-bindkey -M viins '^R'   widget::hist  # input a command from shell's history
-bindkey -M viins '^Z'   widget::CTRL-Z
+bindkey -M viins '^[c'  fzf-cd # cd to a subdirectory
+bindkey -M viins '^[z'  fzf-zlua  # jump to a folder using z.lua
+bindkey -M viins '^R'   fzf-hist  # input a command from shell's history
+bindkey -M viins '^Z'   _Ctrl+z
 bindkey -M viins '\E\E' fuck  # fix last command
+# bindkey -M viins . rationalize-dot
 # NORMAL {{{2
-bindkey -M vicmd su widget/sudo # rerun last command as root
+# bindkey -M vicmd su widget/sudo # rerun last command as root
 bindkey -M vicmd \! edit-command-line # modify the command being input in $EDITOR
 bindkey -M vicmd ZZ quit   # That's a thing you can do in vim
 bindkey -M vicmd cs change-surround
