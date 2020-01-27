4e40899e7dd6a622aad0d906f377e0fb16ed33c9
Jaden Pleasants <jadenpleasants@fastmail.com>
2020-01-27 10:52:22 -0600

    Removed obselete line from profile

--- .config/zsh/profile.zsh
+++ .config/zsh/profile.zsh
@@ -1,3 +1,2 @@
 if (( $+aliases[which] )) unalias which # There's a file fedora comes with that screws up the which builtin
-path[$path[(i)$HOME/.local/lib64/dotnet/tools]]=()
 # vim:ft=zsh fdm=marker
