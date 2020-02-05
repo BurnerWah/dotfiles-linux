abc5b9b1e924c2fd88a05af6a5325efe7aee2e93
Jaden Pleasants <jadenpleasants@fastmail.com>
2020-02-05 10:56:48 -0600

    Misc changes

--- .config/zsh/profile.zsh
+++ .config/zsh/profile.zsh
@@ -1,2 +1,3 @@
 if (( $+aliases[which] )) unalias which # There's a file fedora comes with that screws up the which builtin
+if [[ "$XDG_CURRENT_DESKTOP" == gnome ]] xrdb -merge ~/.Xdefaults
 # vim:ft=zsh fdm=marker
