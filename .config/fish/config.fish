9f0aed473f72550e5d6fe6ca42014b4113b2ab42
Jaden Pleasants <jadenpleasants@fastmail.com>
2020-06-02 11:13:06 -0500

    I think I might switch from stow to chemozi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,3 @@
 z.lua --init fish | source
 set -gx LS_COLORS (vivid generate burner)
 # This was done with psub before but ALE didn't like that
-if ! contains -- $HOMEBREW_PREFIX/share/fish/vendor_completions.d $fish_complete_path
-  and [ $fish_complete_path[-1] = $XDG_DATA_HOME/fish/generated_completions ]
-    set fish_complete_path[-1] $HOMEBREW_PREFIX/share/fish/vendor_completions.d
-    set -a fish_complete_path $XDG_DATA_HOME/fish/generated_completions
-end
