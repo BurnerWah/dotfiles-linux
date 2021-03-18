5e6e71caaf96e5c3c6a01f741bfb0e68898482f4
Jaden Pleasants <jadenpleasants@fastmail.com>
2021-03-18 15:18:17 -0500

    Improved format-nvim config

--- .config/nvim/lua/user/cfg/format-nvim.lua
+++ .config/nvim/lua/user/cfg/format-nvim.lua
@@ -6,46 +6,94 @@
 --
 -- Issues:
 -- - lua-format struggles with block comments (issues #32, #136)
---
-local cmdgen = {
-  prettier = [[prettier -w --parser %s --config-precedence prefer-file --no-semi --single-quote]],
+assert(true)
+
+---Collection of shared tools & tool generators to ensure consistent configurations
+local tool = {
+
+  ---clang-format
+  clang_format = {cmd = {'clang-format -i'}},
+
+  ---clang-tidy
+  clang_tidy = {
+    cmd = {
+      function(file)
+        local build_dir = vim.fn.findfile('compile_commands.json', vim.fn.expand(file .. ':h'))
+        build_dir = (build_dir ~= '' and '-p ' .. build_dir or build_dir)
+        return string.format('clang-tidy --fix --fix-errors %s %s', build_dir, file)
+      end,
+    },
+    tempfile_dir = '/tmp',
+  },
+
+  ---Generate a prettier tool for a parser
+  ---@param parser string The parser to use
+  ---@return table
+  prettier = function(parser)
+    return {
+      cmd = {
+        List {
+          'prettier', '-w', '--parser', parser, '--config-precedence', '--prefer-file', '--no-semi',
+          '--single-quote',
+        }:concat(' '),
+      },
+    }
+  end,
+
+  ---sqlformat
+  sqlformat = {cmd = {function(F) return string.format('sqlformat -r %s -o %s', F, F) end}},
+
+  ---xmllint
+  xmllint = {cmd = {function(F) return string.format('xmllint --format %s -o %s', F, F) end}},
 }
-local function clang_tidy(file)
-  local build_dir = vim.fn.findfile('compile_commands.json', vim.fn.expand(file .. ':h'))
-  return ([[clang-tidy --fix --fix-errors ]] .. (build_dir ~= '' and '-p ' .. build_dir or '') ..
-             file)
-end
 
 require('format').setup {
   ['*'] = {
-    {
-      cmd = {
-        [[sed -i 's/[ \t]*$//']], -- Remove trailing whitespace
-        [[sed -i ':a; /^$/ { $d; N; ba; }']], -- Remove trailing blank lines
-      },
-    },
+    {cmd = {[[sed -i 's/[ \t]*$//']], [[sed -i ':a; /^$/ { $d; N; ba; }']]}},
+    -- Remove trailing whitespace & trailing blank lines
   },
-  c = {{cmd = {clang_tidy}, tempfile_dir = '/tmp'}, {cmd = {'clang-format -i'}}},
+
+  c = {tool.clang_tidy, tool.clang_format},
+
   cmake = {{cmd = {'cmake-format -i'}}},
-  cpp = {{cmd = {clang_tidy}, tempfile_dir = '/tmp'}, {cmd = {'clang-format -i'}}},
-  css = {{cmd = {cmdgen.prettier:format('css')}}},
+
+  cpp = {tool.clang_tidy, tool.clang_format},
+
+  css = {tool.prettier('css')},
+
   go = {{cmd = {'gofmt -w', 'goimports -w'}, tempfile_postfix = '.tmp'}},
-  html = {{cmd = {cmdgen.prettier:format('html')}}},
-  javascript = {{cmd = {cmdgen.prettier:format('javascript')}}},
-  json = {{cmd = {cmdgen.prettier:format('json')}}},
-  jsonc = {{cmd = {cmdgen.prettier:format('json')}}},
-  less = {{cmd = {cmdgen.prettier:format('less')}}},
+
+  html = {tool.prettier('html')},
+
+  javascript = {tool.prettier('javascript')},
+
+  json = {tool.prettier('json')},
+
+  jsonc = {tool.prettier('json')},
+
+  less = {tool.prettier('less')},
+
   lua = {{cmd = {'lua-format -i'}}},
-  markdown = {{cmd = {cmdgen.prettier:format('markdown')}}},
+
+  markdown = {tool.prettier('markdown')},
+
   python = {{cmd = {'isort', 'yapf -i'}}},
+
   rust = {{cmd = {'rustfmt'}}}, -- NOTE This can format multiple files
-  scss = {{cmd = {cmdgen.prettier:format('scss')}}},
+
+  scss = {tool.prettier('scss')},
+
   sh = {{cmd = {'shfmt -i=2 -ci -w'}}},
-  sql = {{cmd = {function(file) return ([[sqlformat -r %s -o %s]]):format(file, file) end}}},
+
+  sql = {tool.sqlformat},
+
   toml = {{cmd = {'taplo format'}}},
-  typescript = {{cmd = {cmdgen.prettier:format('typescript')}}},
-  xml = {{cmd = {function(file) return ([[xmllint --format %s -o %s]]):format(file, file) end}}},
-  yaml = {{cmd = {cmdgen.prettier:format('yaml')}}},
+
+  typescript = {tool.prettier('typescript')},
+
+  xml = {tool.xmllint},
+
+  yaml = {tool.prettier('yaml')},
 }
 
 vim.cmd [[autocmd init BufWritePost * FormatWrite]]
