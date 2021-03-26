3a528338e4542c06558825918163cdf2fe304fcd
Jaden Pleasants <jadenpleasants@fastmail.com>
2021-03-25 23:36:28 -0500

    Pre-commit, formatting

--- .config/gdb/gdbinit.py
+++ .config/gdb/gdbinit.py
@@ -7,13 +7,13 @@ import gdb_stack_inspector
 
 from xdg.BaseDirectory import xdg_cache_home
 
-gdb.execute('set history save')
-gdb.execute('set history filename ' + xdg_cache_home + '/gdb_history')
-gdb.execute('set verbose off')
-gdb.execute('set print pretty on')
-gdb.execute('set print array off')
-gdb.execute('set print array-indexes on')
-gdb.execute('set python print-stack full')
+gdb.execute("set history save")
+gdb.execute("set history filename " + xdg_cache_home + "/gdb_history")
+gdb.execute("set verbose off")
+gdb.execute("set print pretty on")
+gdb.execute("set print array off")
+gdb.execute("set print array-indexes on")
+gdb.execute("set python print-stack full")
 
 gdb_dashboard.Dashboard.start()
 gdb_stack_inspector.StackVisualizer()
