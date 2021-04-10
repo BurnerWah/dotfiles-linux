a767db5bafd73017c872547fe841d7b4a20c7f4b
Jaden Pleasants <jadenpleasants@fastmail.com>
2021-04-10 16:05:06 -0500

    Added fish setup script

---
+++ .config/fish/bootstrap.fish
@@ -0,0 +1,78 @@
+# NOTE: This is not an actual part of my configuration.
+# It's a file which will set up stuff that, IMO, should be in the fish_variables file.
+#
+# Run it after integrating my dotfiles with your global config.
+abbr -a -U -- - 'cd -'
+abbr -a -U -- . source
+abbr -a -U -- ? if
+abbr -a -U -- al la
+abbr -a -U -- b2sum 'openssl blake2b512'
+abbr -a -U -- batp 'bat -p'
+abbr -a -U -- bcdl bandcamp-dl
+abbr -a -U -- bzegrep 'rg -z'
+abbr -a -U -- bzfgrep 'rg -zF'
+abbr -a -U -- bzgrep 'rg -z'
+abbr -a -U -- c++ clang++
+abbr -a -U -- cc clang
+abbr -a -U -- cls clear
+abbr -a -U -- cmd command
+abbr -a -U -- dc cd
+abbr -a -U -- diff delta
+abbr -a -U -- dsks diskus
+abbr -a -U -- egrep rg
+abbr -a -U -- export 'set -x'
+abbr -a -U -- fgrep 'rg -F'
+abbr -a -U -- find fd
+abbr -a -U -- fn function
+abbr -a -U -- fns functions
+abbr -a -U -- fnsv funcsave
+abbr -a -U -- fun function
+abbr -a -U -- func function
+abbr -a -U -- gdl gallery-dl
+abbr -a -U -- grep rg
+abbr -a -U -- h help
+abbr -a -U -- l ls
+abbr -a -U -- lldd 'LD_LIBRARY_PATH=$PWD ldd'
+abbr -a -U -- local 'set -l'
+abbr -a -U -- loop 'while true'
+abbr -a -U -- lzcat 'xzcat -F lzma'
+abbr -a -U -- lzegrep 'rg -z'
+abbr -a -U -- lzfgrep 'rg -zF'
+abbr -a -U -- lzgrep 'rg -z'
+abbr -a -U -- lzma 'xz -F lzma'
+abbr -a -U -- md5sum 'openssl md5'
+abbr -a -U -- nc ncat
+abbr -a -U -- ping4 'ping -4'
+abbr -a -U -- ping6 'ping -6'
+abbr -a -U -- ports 'netstat -ntup'
+abbr -a -U -- psrg 'ps aux | rg'
+abbr -a -U -- py python
+abbr -a -U -- python3 python
+abbr -a -U -- rpmq 'rpm -q'
+abbr -a -U -- sha1sum 'openssl sha1'
+abbr -a -U -- sha224sum 'openssl sha224'
+abbr -a -U -- sha256sum 'openssl sha256'
+abbr -a -U -- sha384sum 'openssl sha384'
+abbr -a -U -- sha512sum 'openssl sha512'
+abbr -a -U -- sqlite sqlite3
+abbr -a -U -- trash 'gio trash'
+abbr -a -U -- unlzma 'unxz -F lzma'
+abbr -a -U -- vi nvim
+abbr -a -U -- vim nvim
+abbr -a -U -- wget 'curl -LO'
+abbr -a -U -- which whereis
+abbr -a -U -- wlcp wl-copy
+abbr -a -U -- wlp wl-paste
+abbr -a -U -- xargs rargs
+abbr -a -U -- xcp 'xsel -b -l /dev/null'
+abbr -a -U -- xopen open
+abbr -a -U -- xzegrep 'rg -z'
+abbr -a -U -- xzfgrep 'rg -zF'
+abbr -a -U -- xzgrep 'rg -z'
+abbr -a -U -- ydl youtube-dl
+abbr -a -U -- ydlmp3 'youtube-dl -x --audio-format mp3'
+abbr -a -U -- yum dnf
+abbr -a -U -- zegrep 'rg -z'
+abbr -a -U -- zfgrep 'rg -zF'
+abbr -a -U -- zgrep 'rg -z'
+abbr -a -U -- zstdgrep 'rg -z'
